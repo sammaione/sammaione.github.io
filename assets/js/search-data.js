a2fcdd05883fcfba7f40ed6e2ec665626bfbcc61
sammaione <138528477+sammaione@users.noreply.github.com>
2025-09-24 02:55:23 +0000

    Deploying to gh-pages from @ sammaione/sammaione.github.io@84c8847b8c405ef9e15f45e77e747fd3a1390823 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
+  },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
